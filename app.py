8d1c10fcf7e1bca300a5170d1899e67dfbc087f2
Giacomo Lanciano <g.lanciano1@gmail.com>
2020-04-29 19:06:25 +0200

    app.py: log scaled payload and prediction output to console

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ def scale(payload):
     LOG.info(f"Scaling Payload: \n{payload}")
     scaler = StandardScaler().fit(payload.astype(float))
     scaled_adhoc_predict = scaler.transform(payload.astype(float))
+    LOG.info(f"Scaled Payload: \n{scaled_adhoc_predict}")
     return scaled_adhoc_predict
 
 
@@ -65,7 +66,8 @@ def predict():
     scaled_payload = scale(inference_payload)
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
-    # TO DO:  Log the output prediction value
+    # Log the output prediction value
+    LOG.info(f"Prediction output: \n{prediction}")
     return jsonify({'prediction': prediction})
 
 
